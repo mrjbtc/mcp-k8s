631a5e346037196818101b3831aecba33923467e
marjune <marjunebatac@gmail.com>
2025-06-15 15:39:39 +0100

    adding test and updating CI config (#6) (#7)
    Co-authored-by: Marjune <marjune@mrjbtc.local>

--- src/tests/test_mcp_response.py
+++ src/tests/test_mcp_response.py
@@ -6,3 +6,8 @@ def test_without_error():
     model = MCPResponse(result={}, error=None)
     assert model.result == {}
     assert model.error == None
+
+def test_with_error():
+    model = MCPResponse(result=None, error="Error")
+    assert model.result == None
+    assert model.error == "Error"
